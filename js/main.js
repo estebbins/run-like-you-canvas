4fead5dedf049b207a4bd7767137da46b19e6af9
estebbins <stebbins.e@gmail.com>
2023-03-28 13:27:56 -0400

    Creates player class and adds beginnings of game loop and gravity

--- js/main.js
+++ js/main.js
@@ -8,3 +8,98 @@
 // The player will have 3 lives. The player will lose a life each time they hit an obstacle
 // After the 3rd life is lost, the next one, the game ends
 // The score will be based off of the distance the player "ran" and the speed at which they ran the course (higher score if same distance but achieved faster)
+
+//! Look into built-in gravity 
+
+
+//////////////////////////////////Set Up///////////////////////////////////////
+// Set HTML elements to variables
+const game = document.getElementById('canvas')
+const message = document.getElementById('message')
+const score = document.getElementById('score')
+const startButton = document.getElementById('start')
+
+// Set game context to 2d
+const ctx = game.getContext('2d')
+
+// Set width & height of the game area based on the browser's size
+game.setAttribute('width', getComputedStyle(game)['width'])
+game.setAttribute('height', getComputedStyle(game)['height'])
+
+// Set minimum game height
+game.height = 400
+
+//////////////////////////Component Classes////////////////////////////////////
+
+// Create a player class to manage the speed and render the square
+class Player {
+    constructor (x, y, width, height, color) {
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+        this.color = color
+        this.alive = true
+        // Since player is stationary at X, they wll hold the frame attribute
+        this.frame = 0
+        // speed will start at medium and increase or decrease - MOVE THIS TO THE OBSTACLES CLASS
+        this.speed = 2
+        this.render = function () {
+            ctx.fillStyle = this.color
+            ctx.fillRect(this.x, this.y, this.width, this.height)
+        }
+        // Gravity/Jumping attributes
+        this.speedX = 0
+        this.speedY = 0
+        this.gravity = 0.0
+        this.gravitySpeed = 0
+        this.newPosition = function () {
+            this.gravitySpeed += this.gravity
+            this.x += this.speedX
+            this.y += this.speedY + this.gravitySpeed
+            this.hitBottom()
+        }
+        this.hitBottom = function () {
+            let bottom = game.height - this.height;
+            if (this.y > bottom) {
+                this.y = bottom
+            }
+        }
+        this.jumpUp = function () {
+            this.gravity = -0.2
+        }
+    }
+}
+
+
+////////////////////////////Game Instances////////////////////////////////////
+// Player starts in lower left corner, but not against the edges
+const player = new Player(45, game.height - 60, 30, 30, 'hotpink')
+player.render()
+
+////////////////////////////////Game Loop/////////////////////////////////////
+
+const gameLoop = () => {
+    // Each loop, add one to the player's frame
+    player.frame += player.speed
+    player.newPosition()
+    player.render()
+}
+
+///////////////////////////////Event Listeners////////////////////////////////
+document.addEventListener('keydown', (e) => {
+    console.log('this is they key', e.key)
+    if(['w', 'W', 'ArrowUp'].includes(e.key)) {
+        player.jumpUp()
+    }
+})
+
+// ArrowUp, ArrowRight, ArrowLeft, ArrowDown
+
+// Game Interval
+const gameInterval = setInterval(gameLoop, 60)
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     gameInterval
+// })
+startButton.addEventListener('click', () => gameInterval, {once: true})
